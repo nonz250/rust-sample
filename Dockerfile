bc148a5df5ceb4958d2e0d1c34ffc9de6b97a7b7
nonz250 <hosaka.non.work@gmail.com>
2021-09-12 04:19:18 +0900

    actix-webをインストールしてWEBアプリとして動作するように変更。cargo-watchをインストールしてホットリロード可能に変更。

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,7 @@
 FROM rust:1.53.0-alpine3.13
 
-RUN ls -al
+RUN apk update
+RUN apk add alpine-sdk
+RUN cargo install cargo-watch
 
 COPY ./app /app
--- app/main.rs
+++ app/main.rs
@@ -1,3 +1,17 @@
-fn main() {
-    println!("Hello, world!");
+use actix_web::{get, App, HttpResponse, HttpServer, Responder};
+
+#[get("/")]
+async fn index() -> impl Responder {
+    HttpResponse::Ok().body("Hello world!")
+}
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    HttpServer::new(|| {
+        App::new()
+            .service(index)
+    })
+    .bind("app:8080")?
+    .run()
+    .await
 }
