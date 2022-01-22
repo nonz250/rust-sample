c428e2885ee3ec7747aa779aa23aca62dd86eefc
nonz250 <hosaka.non.work@gmail.com>
2022-01-23 07:01:53 +0900

    fixed migrations.

--- app/migrations/2022-01-04-150630_create_posts/up.sql
+++
@@ -1,6 +0,0 @@
--- Your SQL goes here
-CREATE TABLE `posts` (
-  `id` int unsigned NOT NULL AUTO_INCREMENT,
-  `title` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
---
+++ app/migrations/2022-01-22-205740_create_posts_table/up.sql
@@ -0,0 +1,5 @@
+CREATE TABLE `posts` (
+  `id` varchar(26) NOT NULL DEFAULT '',
+  `title` varchar(50) NOT NULL DEFAULT '',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
